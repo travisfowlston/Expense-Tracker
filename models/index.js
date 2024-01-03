f3816c325cf27aaf34f95bb031c50c419e95fc6b
Travis Fowlston <travisfowlston@ymail.com>
2024-01-03 14:18:18 -0800

    fixed the models

--- models/index.js
+++ models/index.js
@@ -3,26 +3,25 @@ const Expenses = require('./Expenses');
 const Categories = require('./Categories');
 
 User.hasMany(Expenses, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-    });
-    Expenses.belongsTo(User, {
-    foreignKey: 'user_id'
-    });
-    Expenses.belongsTo(Categories, {
-        foreignKey: 'category_id'
-    });
-    Categories.hasMany(Expenses, {
-        foreignKey: 'category_id',
-        onDelete: 'CASCADE'
-    });
-    
-    //q: what does the code above do?
-    //a: creates a one-to-many relationship between the user and expenses models
-    //q: what does onDelete: 'CASCADE' do?
-    
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
-    
+Expenses.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
+Expenses.belongsTo(Categories, {
+  foreignKey: 'category_id',
+});
 
-module.exports = { User, Expenses, Categories};
+Categories.hasMany(Expenses, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+//q: what does the code above do?
+//a: creates a one-to-many relationship between the user and expenses models
+//q: what does onDelete: 'CASCADE' do?
+
+module.exports = { User, Expenses, Categories };
