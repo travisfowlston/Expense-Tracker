1dbc2b6151d8cc5c3b2370aee0db69be8319e605
Travis Fowlston <travisfowlston@ymail.com>
2024-01-07 22:01:09 -0800

    worked on profile.handlebars, profile.js, and all the routes

--- controllers/api/expenseRoutes.js
+++ controllers/api/expenseRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Expenses, User } = require('../../models');
+const { Expenses } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,4 @@ const homeRoutes = require('./homeRoutes');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-
-
 module.exports = router;
