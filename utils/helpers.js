ebbccc1fc9a9d524d620574789f1baf279e4d4d0
unknown <fullertonlogan@gmail.com>
2024-01-09 18:40:24 -0800

    added code for total amount per category

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,4 @@
-module.exports = {
+const helpers = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
@@ -7,4 +7,22 @@ module.exports = {
     // format large numbers with commas
     return parseInt(amount).toLocaleString();
   },
+  sumByCategory: (category, expenses) => {
+    let sum = 0;
+    if (!expenses) {
+      return sum;
+    }
+    for (let i = 0; i < expenses.length; i++) {
+      if (expenses[i].category === category) {
+        sum += expenses[i].amount;
+      }
+    }
+    return sum;
+  },
 };
+
+// Calculate the sum of expenses by category
+
+
+module.exports = helpers;
+
