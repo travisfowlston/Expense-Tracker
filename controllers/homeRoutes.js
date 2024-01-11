b8dcbcc2a4b98c24e47cab36f5a057b3b43c2fa0
Travis Fowlston <travisfowlston@ymail.com>
2024-01-10 20:55:30 -0800

    final final code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,55 +2,6 @@ const router = require('express').Router();
 const { Expenses, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all expenses and JOIN with user data
-    const reportData = await Expenses.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const expenseReports = reportData.map((report) =>
-      report.get({ plain: true })
-    );
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      expenseReports,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/expenses/:id', async (req, res) => {
-  try {
-    const reportData = await Expenses.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const expenseReports = reportData.get({ plain: true });
-
-    res.render('expenses', {
-      ...expenseReports,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
